15f95eb3c7908c30592421b764b67d724430a4f6
ss2s <ss.samsung86@gmail.com>
2017-09-16 20:08:17 +0300

    2008160917

--- Arduino/sim900_UNO_v2.0/sim900_UNO_v2.0.ino
+++ Arduino/sim900_UNO_v2.0/sim900_UNO_v2.0.ino
@@ -1,9 +1,93 @@
+#define GSM Serial
+//#define LEO Serial1
+#define LED 13
+#define KNOPKA 7 // ПИН ПОДКЛЮЧЕНИЯ КНОПКИ
+#define MODEMBOOT 9
+
+unsigned long pulsVal = 1000000; // ДЛИТЕЛЬНОСТЬ ИМПУЛЬСА В мкс. ДЛИТЕЛЬНОСТЬ НАЖАТИЯ ДЛЯ СРАБОТКИ КНОПКИ
+unsigned long duration;
+unsigned long durationTimeout = 50000000; // ДЛИТЕЛЬНОСТЬ ТАЙМАУТА (ДОЛЖНА БЫТЬ В 50 И БОЛЕЕ РАЗ БОЛЬШЕ ЧЕМ ДЛИТЕЛЬНОСТЬ ИМПУЛЬСА)
+boolean lastLed;
+
+void ring(){
+  GSM.println("ATD+380965116553;"); //// НОМЕР ТЕЛЕФОНА НА КОТОРЫЙ БУДЕТ ЗВОНИТЬ ////
+   
+  delay(2000);
+}
+
+void modemcheck(){
+
+  GSM.println("AT");
+
+  delay(100);
+
+  if (!GSM.find("OK")){
+    digitalWrite(MODEMBOOT, HIGH);            
+    delay(1000);
+    digitalWrite(MODEMBOOT, LOW);
+  }
+  delay(2000);    
+  
+  GSM.println("ATE0"); 
+   
+  while(1){
+      GSM.println("AT+COPS?");
+      if (GSM.find("+COPS: 0")) break;
+ 
+  }
+}
+ 
 void setup() {
-  // put your setup code here, to run once:
 
+  GSM.begin(9600);
+
+
+  delay(2000);
+
+  pinMode(KNOPKA, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
+  pinMode(MODEMBOOT, OUTPUT);
+  digitalWrite(LED, LOW);
+
+  modemcheck();
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
+    
+   bool chk = digitalRead(KNOPKA);
 
+  if(chk == LOW)
+  {
+    modemcheck();
+    digitalWrite(LED, HIGH);
+    ring();
+  }
+
+  // if (LEO.available()>0){
+  //   int inByte = LEO.read();
+  //   GSM.write(inByte);
+  // }
+
+  // if (GSM.available()>0){
+  //   int inByte = GSM.read();
+  //   LEO.write(inByte);
+  // } 
+
+  if (digitalRead(LED)>0){
+
+    delay(200);   // ЕСЛИ БУДЕТ ВЫКЛЮЧАТЬ РЕЛЕ ДО ТОГО МОМЕНТА
+                  // КАК ВЗЯЛИ ТРУБКУ. УВЕЛИЧИТЬ ЗАДЕРЖКУ,ВПЛОТЬ ДО ВРЕМЕНИ
+                  // КОТОРОЕ ИДУТ ГУДКИ (ЕСЛИ РАБОТАЕТ ЛУЧШЕ НЕ ТРОГАТЬ)
+
+    GSM.println("AT+CPAS");
+
+    delay(200);
+
+    if (!GSM.find("4")){
+      delay(1000);
+      digitalWrite(LED, LOW);
+    }
+  }
 }
+ 
+
--- Arduino/sim900_leonardo/sim900_leonardo.ino
+++ Arduino/sim900_leonardo/sim900_leonardo.ino
@@ -4,6 +4,11 @@
 #define KNOPKA 7 // ПИН ПОДКЛЮЧЕНИЯ КНОПКИ
 #define MODEMBOOT 9
 
+unsigned long pulsVal = 1000000; // ДЛИТЕЛЬНОСТЬ ИМПУЛЬСА В мкс. ДЛИТЕЛЬНОСТЬ НАЖАТИЯ ДЛЯ СРАБОТКИ КНОПКИ
+unsigned long duration;
+unsigned long durationTimeout = 50000000; // ДЛИТЕЛЬНОСТЬ ТАЙМАУТА (ДОЛЖНА БЫТЬ В 50 И БОЛЕЕ РАЗ БОЛЬШЕ ЧЕМ ДЛИТЕЛЬНОСТЬ ИМПУЛЬСА)
+boolean lastLed;
+
 void ring(){
   GSM.println("ATD+380965116553;"); //// НОМЕР ТЕЛЕФОНА НА КОТОРЫЙ БУДЕТ ЗВОНИТЬ ////
    
@@ -42,6 +47,7 @@ void setup() {
   pinMode(KNOPKA, INPUT_PULLUP);
   pinMode(LED, OUTPUT);
   pinMode(MODEMBOOT, OUTPUT);
+  digitalWrite(LED, LOW);
 
   modemcheck();
 }
